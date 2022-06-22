6f7a215e1eafdd81e423d6355e4aba1e64d06af6
mohamed bekrine <mohamed.bekrine.mb@gmail.com>
2022-06-22 18:08:31 +0100

    first-commit

---
+++ src/compennets/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <span>Logo</span>
+            <div>
+                <a>
+                    films
+                </a>
+                <a>
+                    en streaming
+                </a>
+            </div>
+        </div>
+        <div>
+            <span>
+                recherch
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
